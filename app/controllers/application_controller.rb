87e7ebd2f5b5fb60a0f8dae7f3952c752d498d30
sonots <sonots@gmail.com>
2013-05-22 00:30:17 +0900

    rescue_from

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from GrowthForecast::NotFound do |e|
+    render json: {'message' => e.message}, status: 404
+  end
+  rescue_from GrowthForecast::AlreadyExists do |e|
+    render json: {'message' => e.message}, status: 409
+  end
+  rescue_from GrowthForecast::Error do |e|
+    render json: {'message' => e.message}, status: :unprocessable_entity
+  end
 end
--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -4,39 +4,29 @@ class GraphsController < ApplicationController
   # GET /graphs
   def index
     @graphs = $mgclient.list_graph
-  rescue GrowthForecast::Error => e
-    render json: {'message' => e.message}, status: :unprocessable_entity
   end
 
   # GET /graphs/:path
   def show
     @graph = $mgclient.get_graph(params[:path])
-  rescue GrowthForecast::Error => e
-    render json: {'message' => e.message}, status: :unprocessable_entity
   end
 
   # POST /graphs
   def create
     @graph = $mgclient.post_graph(params[:path], create_params)
     render action: 'show', status: :created
-  rescue GrowthForecast::Error => e
-    render json: {'message' => e.message}, status: :unprocessable_entity
   end
 
   # PUT /graphs/:path
   def update
     @graph = @mgclient.edit_graph(params[:path], update_params)
     render action: 'show', status: :updated
-  rescue GrowthForecast::Error => e
-    render json: {'message'=> e.message}, status: :unprocessable_entity
   end
 
   # DELETE /graphs/:path
   def destroy
     @graph = $mgclient.delete_graph(params[:path])
     render action: 'show', status: :updated
-  rescue GrowthForecast::Error => e
-    render json: {'message'=> e.message}, status: :unprocessable_entity
   end
 
   private
