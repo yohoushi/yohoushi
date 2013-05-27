e100915c8ef3601c40286fdb537c9b99fb756bc2
sonots <sonots@gmail.com>
2013-05-27 18:21:43 +0900

    crete and destroy with growthforecast

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,10 +1,10 @@
 class GraphsController < ApplicationController
   before_action :set_graph, only: [:show, :edit, :update, :destroy]
+  before_action :set_graphs, only: [:index]
 
   # GET /graphs
   # GET /graphs.json
   def index
-    @graphs = Graph.all
   end
 
   # GET /graphs/1
@@ -24,10 +24,14 @@ class GraphsController < ApplicationController
   # POST /graphs
   # POST /graphs.json
   def create
-    @graph = Graph.new(graph_params)
+    success = ActiveRecord::Base.transaction do
+      @graph = Graph.new(graph_params)
+      @graph.save
+      $mgclient.post_graph(@graph.path, create_params)
+    end
 
     respond_to do |format|
-      if @graph.save
+      if success
         format.html { redirect_to @graph, notice: 'Graph was successfully created.' }
         format.json { render action: 'show', status: :created, location: @graph }
       else
@@ -40,8 +44,13 @@ class GraphsController < ApplicationController
   # PATCH/PUT /graphs/1
   # PATCH/PUT /graphs/1.json
   def update
+    success = ActiveRecord::Base.transaction do
+      @graph.update(graph_params)
+      $mgclient.edit_graph(@graph.path, update_params)
+    end
+
     respond_to do |format|
-      if @graph.update(graph_params)
+      if success
         format.html { redirect_to @graph, notice: 'Graph was successfully updated.' }
         format.json { head :no_content }
       else
@@ -54,7 +63,10 @@ class GraphsController < ApplicationController
   # DELETE /graphs/1
   # DELETE /graphs/1.json
   def destroy
-    @graph.destroy
+    success = ActiveRecord::Base.transaction do
+      @graph.destroy
+      @graph = $mgclient.delete_graph(@graph.path) rescue nil
+    end
     respond_to do |format|
       format.html { redirect_to graphs_url }
       format.json { head :no_content }
@@ -62,13 +74,26 @@ class GraphsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_graph
-      @graph = Graph.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def graph_params
-      params.require(:graph).permit(:path)
-    end
+  def create_params
+    {:number => 0}
+  end
+
+  def update_params
+    {}
+  end
+
+  def set_graphs
+    @graphs = Graph.all
+  end
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_graph
+    @graph = Graph.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def graph_params
+    params.require(:graph).permit(:path)
+  end
 end
