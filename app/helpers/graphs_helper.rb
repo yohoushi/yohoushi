9a9ca81436a3d9cf589a7d2c9cbc4c7878de0af6
sonots <sonots@gmail.com>
2013-06-07 20:47:49 +0900

    def tree

--- app/helpers/graphs_helper.rb
+++ app/helpers/graphs_helper.rb
@@ -1,3 +1,28 @@
 module GraphsHelper
   include ActsAsTaggableOn::TagsHelper
+
+  def tree(root)
+    html = ''
+    open_ul = 0
+    prev_depth = root.depth - 1
+    root.subtree.each do |path|
+      curr_depth = path.depth
+      diff = curr_depth - prev_depth
+      if curr_depth > prev_depth
+        html += '<ul><li style="list-style:none">' * (diff - 1).abs
+        html += '<ul><li>'
+        open_ul += diff
+      elsif curr_depth == prev_depth
+        html += '</li><li>'
+      elsif curr_depth < prev_depth
+        html += '</li></ul>' * diff.abs
+        html += '</li><li>'
+        open_ul -= diff
+      end
+      prev_depth = curr_depth
+      html += link_to(path.fullpath, view_graph_path(path.fullpath))
+    end
+    html += '</li></ul>' * open_ul.abs
+    html
+  end
 end
--- app/models/path.rb
+++ app/models/path.rb
@@ -3,9 +3,13 @@ class Path < ActiveRecord::Base
 
   # @return id of the direct parent
   def self.create_parents(fullpath)
-    return nil if (dirname = File.dirname(fullpath)) == '.'
-    parent = create_parents(dirname)
-    # NOTE: where(:fullpath, :parent_id).first_or_create can not be used since :parent_id is not a real column
-    Directory.select(:id).where(:fullpath => dirname).first || Directory.create(:fullpath => dirname, :parent => parent)
+    if (dirname = File.dirname(fullpath)) == '.'
+      parent = Directory.select(:id).where(:fullpath => '/').first || Directory.create(:fullpath => '/')
+    else
+      parent_id = create_parents(dirname)
+      # NOTE: where(:fullpath, :parent_id).first_or_create can not be used since :parent_id is not a real column
+      parent = Directory.select(:id).where(:fullpath => dirname).first || Directory.create(:fullpath => dirname, :parent_id => parent_id)
+    end
+    parent.id
   end
 end
