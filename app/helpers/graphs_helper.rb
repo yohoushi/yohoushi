72634966e13557324528c6e9eba43a6111426a98
sonots <sonots@gmail.com>
2013-06-18 16:00:03 +0900

    fix graph tree order

--- app/helpers/graphs_helper.rb
+++ app/helpers/graphs_helper.rb
@@ -13,7 +13,7 @@ module GraphsHelper
 
   def tree(root, depth = 5)
     return '' unless root
-    subtree = depth.present? ? root.subtree(to_depth: depth) : root.subtree
+    subtree = (depth.present? ? root.subtree(to_depth: depth) : root.subtree).order('path ASC')
     html = ''
     open_ul = 0
     prev_depth = root.depth - 1
