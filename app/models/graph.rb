666489daf65f74e24422613765386b6411bc074c
sonots <sonots@gmail.com>
2013-06-20 22:39:50 +0900

    auto tagging

--- app/models/graph.rb
+++ app/models/graph.rb
@@ -1,3 +1,14 @@
 class Graph < Node
   acts_as_taggable_on :tags
+
+  # Find or create a graph with its ancestors
+  #
+  # @param [params] parameters of the node
+  # @return [Graph] graph object
+  def self.find_or_create(params)
+    super.tap do |graph|
+      graph.tag_list.add params[:path].split('/')
+      graph.save!
+    end
+  end
 end
