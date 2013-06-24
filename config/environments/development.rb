47867cd0ccbcc6fd5cda2954ee1e45583450c67c
sonots <sonots@gmail.com>
2013-06-24 12:40:17 +0900

    allow_ip private ip ranges

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,10 @@ Yohoushi::Application.configure do
     Bullet.bullet_logger = true
     Bullet.console = true
     Bullet.rails_logger = true
-    Bullet.disable_browser_cache = true
+
+    # allow all private ip ranges
+    BetterErrors::Middleware.allow_ip! '10.0.0.0/8'
+    BetterErrors::Middleware.allow_ip! '172.16.0.0/12'
+    BetterErrors::Middleware.allow_ip! '192.168.0.0/16'
   end
 end
