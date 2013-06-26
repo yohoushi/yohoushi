19a89fe952d3126d9bc05b6196a7c0f3ea2b09a1
sonots <sonots@gmail.com>
2013-06-26 21:42:57 +0900

    should support more, otherwise worker would error

--- db/migrate/20130605091736_create_nodes.rb
+++ db/migrate/20130605091736_create_nodes.rb
@@ -2,7 +2,7 @@ class CreateNodes < ActiveRecord::Migration
   def change
     create_table :nodes do |t|
       t.string :type # STI
-      t.string :path, limit: 255, index: true, null: false, collation: 'utf8_bin'
+      t.string :path, limit: 255*3+3, index: true, null: false, collation: 'utf8_bin' # note that index works upto 255
       t.string :description
       t.boolean :hidden
       t.boolean :complex
