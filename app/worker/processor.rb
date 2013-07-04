12eedd5c8848227659040c79bb9e1963007e93e0
sonots <sonots@gmail.com>
2013-07-05 02:12:53 +0900

    was wrong, fixed completely

--- app/worker/processor.rb
+++ app/worker/processor.rb
@@ -11,28 +11,31 @@ module Worker
 
     def process
       report_time(logger) do
-        graphs = $mfclient.list_graph.map {|g| g['path'] }
-        in_batches(graphs, batch_size: 1000) do |batches|
-          create_or_destroy(batches)
+        graphs    = $mfclient.list_graph.map   {|g| g['path'] }
+        complexes = $mfclient.list_complex.map {|g| g['path'] }
+        both      = graphs + complexes
+
+        # diff
+        plus  = both
+        minus = []
+        Graph.select('id, path').find_in_batches(batch_size: 1000) do |batches|
+          batches = batches.map(&:path)
+          plus   -= batches
+          minus  += batches - both
         end
 
-        complexes = $mfclient.list_complex.map {|g| g['path'] }
-        in_batches(complexes, batch_size: 1000) do |batches|
-          create_or_destroy(batches, complex: true)
-        end
+        # create
+        complex_plus = plus & complexes
+        graph_plus   = plus - complex_plus
+        complex_plus.each {|path| Graph.find_or_create(path: path, complex: true) }
+        graph_plus.each {|path| Graph.find_or_create(path: path, complex: false) }
+
+        # destroy
+        minus.each {|path| Graph.find_by(path: path).destroy_ancestors }
       end
     rescue => e
       logger.error "#{e.class} #{e.message} #{e.backtrace.first}"
       raise e # die, but `god` will reboot it
-    end
-
-    def create_or_destroy(paths, complex: false)
-      selected = sql.execute("select path from nodes where type = 'Graph' AND path IN #{in_clause(paths)}").to_a.flatten
-      added   = paths - selected
-      removed = selected - paths
-      added.each {|add| Graph.find_or_create(path: add, complex: complex) }
-      removed.each {|remove| Graph.find_by(path: remove).destroy_ancestors }
-      nil
     end
   end
 end
--- lib/util.rb
+++ lib/util.rb
@@ -9,23 +9,4 @@ module Util
     logger.debug "Elapsed time: %.2f seconds at %s" % [(Time.now - t).to_f, caller()[0]]
     output
   end
-
-  # just alias
-  def sql
-    ActiveRecord::Base.connection
-  end
-
-  # Convert array of strings to `("a", "b")` format to use at IN clause of SQL
-  def in_clause(array)
-    str = array.inspect # ["a", "b"]
-    "(#{str[1, str.size-2]})" # ("a", "b")
-  end
-
-  # Process block in batches (like ActiveRecord#find_in_batches)
-  def in_batches(array, start: 0, batch_size: 1000, &blk)
-    last = array.size - 1
-    (start..last).step(batch_size).each do |start|
-      yield array[start, batch_size]
-    end
-  end
 end
