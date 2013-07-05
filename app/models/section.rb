ace9d25cf4131cae99ef875f6c37ef16c15aaeaf
sonots <sonots@gmail.com>
2013-07-05 15:29:16 +0900

    spec for destroy_all_childless

--- app/models/section.rb
+++ app/models/section.rb
@@ -2,7 +2,7 @@ class Section < Node
   # Destory all childless sections
   def self.destroy_all_childless(batch_size: 1000)
     Section.select('id, ancestry').find_each(batch_size: batch_size) do |section|
-      section.destroy unless section.children.first.present?
+      section.destroy_ancestors unless section.children.first.present?
     end
   end
 end
--- spec/models/section_spec.rb
+++ spec/models/section_spec.rb
@@ -2,7 +2,24 @@ require 'spec_helper'
 
 describe Section do
   describe "#destroy_all_childless" do
+    context "default" do
+      before do
+        Graph.find_or_create(path: 'a/b/c').destroy
+        Section.destroy_all_childless 
+      end
+      it { expect(Section.find_by(path: 'a/b')).to be_nil }
+      it { expect(Section.find_by(path: 'a')).to be_nil }
+      it { expect(Section.find_by(path: '')).to be_nil }
+    end
 
+    context "section having a child is not destroyed" do
+      before do
+        Graph.find_or_create(path: 'a/b/c').destroy
+        Graph.find_or_create(path: 'a/b/d') # not destroy
+        Section.destroy_all_childless 
+      end
+      it { expect(Section.find_by(path: 'a/b')).not_to be_nil }
+    end
   end
 end
 
