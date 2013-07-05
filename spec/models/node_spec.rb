39f780332f326befdab78ae2676c4835dad5121b
sonots <sonots@gmail.com>
2013-07-05 15:02:57 +0900

    remove marking spec

--- spec/models/node_spec.rb
+++ spec/models/node_spec.rb
@@ -10,42 +10,14 @@ describe Node do
   end
 
   describe "#find_or_create" do
-    context 'default' do
-      let(:path) { 'a/b/c' }
-      subject { described_class.find_or_create(path: path) }
-      it { expect(subject.path).to eq 'a/b/c' }
-      it { expect(subject.parent.path).to eq 'a/b' }
-      it { expect(subject.parent.parent.path).to eq 'a' }
-    end
+    let(:path) { 'a/b/c' }
+    subject { described_class.find_or_create(path: path) }
+    it { expect(subject.path).to eq 'a/b/c' }
+    it { expect(subject.parent.path).to eq 'a/b' }
+    it { expect(subject.parent.parent.path).to eq 'a' }
+  end
 
-    context 'marking then delete non-marked nodes' do
-      let(:path) { 'a/b/c' }
-      before {
-        @marks = described_class.start_marking
-        described_class.find_or_create(path: path)
-        described_class.where.not(:id => @marks.uniq).destroy_all
-      }
-      it { expect(@marks).to include(described_class.find_by(path: '').id) }
-      it { expect(@marks).to include(described_class.find_by(path: 'a').id) }
-      it { expect(@marks).to include(described_class.find_by(path: 'a/b').id) }
-      it { expect(@marks).to include(described_class.find_by(path: 'a/b/c').id) }
-      it { expect(described_class.all.map(&:path)).to eq ['', 'a', 'a/b', 'a/b/c'] }
-    end
-
-    context 'default then marking then delete non-marked nodes' do
-      let(:path1) { 'a/b/c' }
-      let(:path2) { 'a/d/e' }
-      before {
-        described_class.find_or_create(path: path1) # means a graph which does not exist in gf anymore
-        @marks = described_class.start_marking
-        described_class.find_or_create(path: path2) # means a graph which exists in gf
-        described_class.where.not(:id => @marks.uniq).destroy_all
-      }
-      it { expect(@marks).to include(described_class.find_by(path: '').id) }
-      it { expect(@marks).to include(described_class.find_by(path: 'a').id) }
-      it { expect(@marks).to include(described_class.find_by(path: 'a/d').id) }
-      it { expect(@marks).to include(described_class.find_by(path: 'a/d/e').id) }
-      it { expect(described_class.all.map(&:path)).to eq ['', 'a', 'a/d', 'a/d/e'] }
-    end
+  describe "#destroy_ancestors" do
+    pending
   end
 end
---
+++ spec/models/section_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe Section do
+  describe "#destroy_all_childless" do
+
+  end
+end
+
