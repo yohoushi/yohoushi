3a2429e395dbefade44ee12c3bb4084c63b66518
sonots <sonots@gmail.com>
2013-07-05 14:57:56 +0900

    Section#destroy_all_childless

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,2 +1,8 @@
 class Section < Node
+  # Destory all childless sections
+  def self.destroy_all_childless(batch_size: 1000)
+    Section.select('id, ancestry').find_each(batch_size: batch_size) do |section|
+      section.destroy unless section.children.first.present?
+    end
+  end
 end
