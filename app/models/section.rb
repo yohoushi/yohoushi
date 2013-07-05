23b2fd1cebdce553b08f8166bf2123ca2d45f301
sonots <sonots@gmail.com>
2013-07-05 15:54:57 +0900

    comment

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,5 +1,6 @@
 class Section < Node
   # Destory all childless sections
+  # NOTE: This method is not called from anywhere, but let me keep because I may use this from rails console
   def self.destroy_all_childless(batch_size: 1000)
     Section.select('id, ancestry').find_each(batch_size: batch_size) do |section|
       section.destroy_ancestors unless section.children.first.present?
