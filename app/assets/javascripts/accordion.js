b6fe23b4af6168125e3baf25945a7dd3a497608d
Nobuhiro Nikushi <nikushi.nobuhiro@dena.jp>
2013-07-08 21:06:16 +0900

    Remove icons for now

--- app/assets/javascripts/accordion.js
+++ app/assets/javascripts/accordion.js
@@ -12,9 +12,8 @@ $(function() {
       .done(function(data) {
         $.each(data, function(i,item) {
           var div_class = (item.has_children == true) ? 'class="accordion-head"' : ''
-          var icon_right = (item.has_children == true) ? '' : '<i class="icon-chevron-right"></i>'
           var arrow = (item.has_children == true) ?  '<span class="accordion-nav-arrow"></span>' : ''
-          ul.append('<li><div ' + div_class + ' data-term="' + item.path + '"><a href="' + item.uri + '" >' + icon_right + ' ' + item.basename + '</a>' + arrow + '</div><ul class="accordion-nav" style="display: none;"></ul></li>');
+          ul.append('<li><div ' + div_class + ' data-term="' + item.path + '"><a href="' + item.uri + '" > ' + item.basename + '</a>' + arrow + '</div><ul class="accordion-nav" style="display: none;"></ul></li>');
         });
 
         // Add a class to ul tag if creat the last leaf box
