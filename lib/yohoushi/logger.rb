dd6a6ca931d207c575623bf99a0050da947bd70b
sonots <sonots@gmail.com>
2013-07-08 18:44:11 +0900

    Yohoushi.logger

---
+++ lib/yohoushi/logger.rb
@@ -0,0 +1,41 @@
+require 'logger'
+require 'active_support'
+
+module Yohoushi
+  module_function
+
+  # Create a logger instance
+  #
+  # @param out This is a filename (String) or IO object (typically STDOUT, STDERR, or an open file).
+  # @param level Log level
+  # @param shift_age Number of old log files to keep, or frequency of rotation (daily, weekly or monthly).
+  # @param shift_size Maximum logfile size (only applies when shift_age is a number).
+  # @param config The config yaml file to load
+  # @param block Format block
+  # @return Logger instance
+  def logger(params = { out: $stdout, level: 'info', shift_age: 0, shift_size: 1048676, config: nil }, &block)
+    params = params.symbolize_keys
+    # Load the config yaml
+    if params[:config] and File.exists?(params[:config])
+      params = YAML.load_file(params[:config])[Rails.env]["logger"].symbolize_keys.merge(params)
+    end
+
+    # params[:out]
+    if params[:out].kind_of?(String) # String
+      params[:out] = File.expand_path(params[:out], Rails.root)
+    elsif params[:out].respond_to?(:sync) # IO object
+      params[:out].sync = true
+    end
+
+    # Default log formart
+    block ||= proc do |level, time, _, message|
+      "[#{time.strftime("%Y-%m-%d %H:%M:%S")}] [#{level.rjust(5)}] #{message}\n"
+    end
+
+    # Make an instance of logger
+    Logger.new(params[:out], params[:shift_age], params[:shift_size]).tap do |logger|
+      logger.formatter = block
+      logger.level = eval("Logger::#{params[:level].upcase}")
+    end
+  end
+end
