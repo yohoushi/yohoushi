1751b9322b8752f2a1b9d176e554e71e16ce5c01
sonots <sonots@gmail.com>
2013-07-10 21:45:34 +0900

    assets:precompile in bin/yohoushi

--- lib/yohoushi/cli.rb
+++ lib/yohoushi/cli.rb
@@ -139,9 +139,13 @@ begin
     end
   else
     puts "Sending output to log file: #{File.expand_path('log/application.log', ENV['RAILS_ROOT'])}" # yohoushi custom
+    th = Thread.new do
+      system "#{RAILS_ROOT}/bin/rake assets:precompile" # yohoushi custom
+    end
     require 'god/cli/run'
     require File.expand_path('vendor/extensions/god/cli/run', ENV['RAILS_ROOT']) # yohoushi custom
     God::CLI::Run.new(options)
+    th.join
   end
 rescue Exception => e
   if e.instance_of?(SystemExit)
