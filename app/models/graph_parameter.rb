c261429b5b54b88c65401b55df0f0475907cd78a
Nobuhiro Nikushi <nikushi.nobuhiro@dena.jp>
2013-07-11 18:48:00 +0900

    Fix short_metrics bug

--- app/models/graph_parameter.rb
+++ app/models/graph_parameter.rb
@@ -34,7 +34,7 @@ class GraphParameter < ApplicationParameter
   # query parameters passed to growthforecast's graph image uri
   def graph_uri_params
     params = {
-      't'      => @t,
+      't'      => gf_term,
       'from'   => @from,
       'to'     => @to,
       'width'  => @width,
@@ -47,5 +47,12 @@ class GraphParameter < ApplicationParameter
   def validate
     self.errors.add(:from, 'must be older than `to`.') if @from and @to and @from >= @to
     self
+  end
+
+  private
+
+  def gf_term
+    short_metrics = Settings.multiforecast.try(:short_metrics).nil? || Settings.multiforecast.try(:short_metrics)
+    (short_metrics && %w(c h 4h n 8h d 3d).include?(@t)) ? "s#{@t}" : @t
   end
 end
