faddac5249a665ad0d7d93900a8cc933bba76c3d
sonots <sonots@gmail.com>
2013-07-16 16:08:35 +0900

    add spec

--- spec/models/graph_parameter_spec.rb
+++ spec/models/graph_parameter_spec.rb
@@ -15,6 +15,82 @@ describe GraphParameter do
     @graph_parameter = described_class.new(:from => from, :to => to, :t => term, :size => size)
   end
 
+  describe "#initialize" do
+    subject { @graph_parameter }
+
+    context "default" do
+      subject { @graph_parameter = described_class.new }
+      its(:term) { should == 'd' }
+      its(:from) { should be_nil }
+      its(:to)   { should be_nil }
+      its(:size) { should == 'M' }
+    end
+
+    context "valid" do
+      it { expect(subject.errors[:from]).to be_empty }
+    end
+
+    context ":from equals :to" do
+      let(:from) { later }
+      let(:to)   { later }
+      it { expect(subject.errors[:from].first).to eq 'must be older than `to`.' }
+    end
+
+    context ":from is future than :to" do
+      let(:from) { later }
+      let(:to)   { past }
+      it { expect(subject.errors[:from].first).to eq 'must be older than `to`.' }
+    end
+  end
+
+  describe "#update" do
+    before { @graph_parameter_to_update = described_class.new(:from => from, :to => to, :t => term, :size => size) }
+
+    context "from valid paramters" do
+      context "empty update should keep variables" do
+        subject { @graph_parameter_to_update.update({}) }
+        its(:term) { should == @graph_parameter.term }
+        its(:from) { should == @graph_parameter.from }
+        its(:to)   { should == @graph_parameter.to }
+        its(:size) { should == @graph_parameter.size }
+      end
+
+      context "valid update" do
+        subject { @graph_parameter_to_update.update(:from => past, :to => later) }
+        its(:errors) { should be_empty }
+      end
+
+      context "invalid update should add errors" do
+        subject { @graph_parameter_to_update.update(:from => later, :to => past) }
+        it { expect(subject.errors[:from].first).to eq 'must be older than `to`.' }
+      end
+    end
+
+    context "from invalid parameters" do
+      let(:from) { later }
+      let(:to)   { past }
+
+      context "empty update should keep variables and errors" do
+        subject { @graph_parameter_to_update.update({}) }
+        its(:term) { should == @graph_parameter.term }
+        its(:from) { should == @graph_parameter.from }
+        its(:to)   { should == @graph_parameter.to }
+        its(:size) { should == @graph_parameter.size }
+        it { expect(subject.errors.full_messages).to eq @graph_parameter.errors.full_messages }
+      end
+
+      context "valid update should remove errors" do
+        subject { @graph_parameter_to_update.update(:from => past, :to => later) }
+        its(:errors) { should be_empty }
+      end
+
+      context "invalid updated should remove previous errors and add new errors" do
+        subject { @graph_parameter_to_update.update(:from => later, :to => past) }
+        it { expect(subject.errors[:from].first).to eq 'must be older than `to`.' }
+      end
+    end
+  end
+
   describe "#from_at" do
     subject { @graph_parameter.from_at }
     context "default" do 
@@ -88,23 +164,6 @@ describe GraphParameter do
           it { expect(subject['t']).to eq "#{t}" }
         end
       end
-    end
-  end
-
-  describe "#validate" do
-    subject { @graph_parameter.validate }
-    context "default" do 
-      it { expect(subject.errors[:from]).to be_empty }
-    end
-    context ":from equals :to" do 
-      let(:from) { later }
-      let(:to)   { later }
-      it { expect(subject.errors[:from].first).to eq 'must be older than `to`.' }
-    end
-    context ":from is future than :to" do 
-      let(:from) { later }
-      let(:to)   { past }
-      it { expect(subject.errors[:from].first).to eq 'must be older than `to`.' }
     end
   end
 
