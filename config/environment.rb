7afd5bc45b3be1dc4c9046f14b3da9bcf06df6e5
sonots <sonots@gmail.com>
2013-08-07 01:31:51 +0900

    if RAILS_ENV.rb does not exist

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,11 @@ require File.expand_path('../application', __FILE__)
 require File.expand_path("#{Rails.root}/lib/yohoushi/logger")
 Rails.logger = Yohoushi.logger(out: 'log/application.log', shift_age: 3) # shift_age: 0 to stop logrotate
 
-# Initialize the Rails application.
+# Hack: Load production.rb as default if #{ENV['RAILS_ENV']}.rb does not exist
 unless %w[production development test].include?(ENV['RAILS_ENV'])
-  require File.expand_path("#{Rails.root}/config/environments/production")
+  unless File.exists?(File.expand_path("#{Rails.root}/config/environments/#{ENV['RAILS_ENV']}.rb"))
+    require File.expand_path("#{Rails.root}/config/environments/production")
+  end
 end
+# Initialize the Rails application.
 Yohoushi::Application.initialize!
