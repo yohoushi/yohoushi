1b2fa522ea546d03fa7cda1809d979fde5b33d9d
Nobuhiro Nikushi <deneb.ge@gmail.com>
2013-09-03 19:22:56 +0900

    Refactor

--- app/decorators/node_decorator.rb
+++ app/decorators/node_decorator.rb
@@ -19,22 +19,28 @@ class NodeDecorator < ApplicationDecorator
     end
   end
 
+  # Build HTML lists for the accordion on the top page. The list begins at [self]
+  #
+  # @return [String]
   def build_accordion
     out = ''
-    out += "<ul id='accordion.accordion-nav' class='accordion-nav'>"
-    out += self.decorate.build_accordion_item_with_remain_depth(3) do |node, remain_depth|
+    before_tag = "<ul id='accordion' class='accordion-nav'>"
+    after_tag  = "</ul>"
+    out += self.decorate.accordion_item_with_remain_depth(3, before_tag, after_tag) do |node, remain_depth|
       if node.is_a? Graph
-        node.decorate.accordion_node_graph
+        node.decorate.accordion_graph_node
       elsif remain_depth > 1
-        node.decorate.accordion_node_opened
+        node.decorate.accordion_opened_node
       else
-        node.decorate.accordion_node_closed
+        node.decorate.accordion_closed_node
       end
     end
-    out += "</ul>"
   end
 
-  def accordion_node_opened
+  # Return an opened node of the accordion.
+  #
+  # @return [String]
+  def accordion_opened_node
     out = <<-EOS
       <div class='accordion-head' data-path='#{h.h(self.path)}'>
         <a href="#{h.list_graph_path(self.path)}">#{self.basename}</a>
@@ -43,7 +49,10 @@ class NodeDecorator < ApplicationDecorator
     EOS
   end
 
-  def accordion_node_closed
+  # Return a closed node of the accordion.
+  #
+  # @return [String]
+  def accordion_closed_node
     out = <<-EOS
       <div class='accordion-head' data-path='#{h.h(self.path)}'>
         <a href="#{h.list_graph_path(self.path)}">#{self.basename}</a>
@@ -52,7 +61,10 @@ class NodeDecorator < ApplicationDecorator
     EOS
   end
 
-  def accordion_node_graph
+  # Return a graph node of the accordion.
+  #
+  # @return [String]
+  def accordion_graph_node
     out = <<-EOS
       <div class='accordion-nav-leaf' data-path='#{h.h(self.path)}'>
         <a href="#{h.list_graph_path(self.path)}">#{self.basename}</a>
@@ -61,17 +73,25 @@ class NodeDecorator < ApplicationDecorator
     EOS
   end
 
-  def build_accordion_item_with_remain_depth(remain_depth = 1, &blk)
+  # Build the lists for the accordion recursively.
+  # @param remain_depth [Integer] how much depth to create 
+  # @param before_tag [String] put the string before <li>..</li>. Basically, '<ul>' is given.
+  # @param after_tag [String] put the string after <li>..</li>. Basically, '<ul>' is given.
+  # @parma &blk Block must be givien, be HTML string. It is inserted to the inside of <li>.
+  #
+  # @return [String]
+  def accordion_item_with_remain_depth(remain_depth = 1, before_tag = '', after_tag = '', &blk)
     out = ''
-    return '' if remain_depth == 0
-    self.children.each do |node|
-      out += '<li>'
-      out += blk.call(node, remain_depth)
-      out += "<ul class='accordion-nav'>"
-      out += build_accordion_item_with_remain_depth(remain_depth - 1, &blk)
-      out += "</ul>"
-      out += "</li>"
+    out += before_tag
+    if remain_depth > 0
+      self.children.each do |node|
+        out += '<li>'
+        out += blk.call(node, remain_depth)
+        out += node.decorate.accordion_item_with_remain_depth(remain_depth - 1, "<ul class='accordion-nav'>", "</ul>", &blk)
+        out += "</li>"
+      end
     end
+    out += after_tag
     out
   end
 
