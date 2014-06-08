e109eb64aee4062be6b686e538c0fdb389fb89e9
Nobuhiro Nikushi <deneb.ge@gmail.com>
2014-06-08 16:34:53 +0900

    Implement Api::Yohoushi::GraphsController

--- app/controllers/api/yohoushi/graphs_controller.rb
+++ app/controllers/api/yohoushi/graphs_controller.rb
@@ -1,6 +1,37 @@
 module Api
 module Yohoushi
-  class GraphsController
+  class GraphsController < ApplicationController
+    before_action :set_graph, only: [:show, :update]
+
+    # GET /graphs
+    def index
+      @graphs = Graph.all.order('path ASC')
+    end
+
+    # GET /graphs/:path
+    def show
+    end
+
+    # PATCH/PUT /graphs/:path
+    def update
+      if @graph.update(graph_params)
+        render action: :show
+      else
+        render json: @graph.errors, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def set_graph
+      @graph = Graph.find_by_path(params[:path])
+      not_found unless @graph
+    end
+
+    def graph_params
+      params.permit(:description, :tag_list, :visible)
+    end
+
   end
 end
 end
